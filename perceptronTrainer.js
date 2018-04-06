02d8fe271edc1f6da13ccd1f8b15fa4561ca85f3
Cameron Probert <c.probert@hotmail.co.nz>
2018-04-06 17:25:39 +1200

    finished base code, looking for bugs

--- perceptronTrainer.js
+++ perceptronTrainer.js
@@ -1,7 +1,9 @@
 const Perceptron = require('./perceptron')
 
-let a = 0
-let b = 0
+const maxIterations = 1000
+const learningRate = 0.5
+const a = 0.5
+const b = -10
 
 /**
  * Returns the y value of the function ax + b
@@ -12,11 +14,32 @@ function test (x) {
 }
 
 function isAboveLine (x, y) {
-  test(x) < y ? 0 : 1
+  return test(x) < y ? 0 : 1
 }
 
-function train (perceptron, maxIterations, learningRate) {
+function train (perceptron) {
+  let numCorrect = 0
   for (let i = 0; i < maxIterations; i++) {
-    
+    const point = [
+      Math.random() * 201 - 101,
+      Math.random() * 201 - 101
+    ]
+
+    const actual = perceptron.process(point)
+    const expected = isAboveLine(point[0], point[1])
+    if (actual === expected) numCorrect++
+    if (i % 50 === 0) console.log(`Correct: ${numCorrect}/${i}`)
+    const difference = expected - actual
+
+    perceptron.adjust(point, difference, learningRate)
   }
+}
+
+function trainPerceptron () {
+  const perceptron = new Perceptron(1)
+  train(perceptron)
+}
+
+module.exports = {
+  trainPerceptron
 }
