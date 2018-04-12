a4383b2d2c4cf98ea47ef2bb465be98057a88aa3
Cameron Probert <c.probert@hotmail.co.nz>
2018-04-12 16:24:47 +1200

    started work on Points object

---
+++ client/components/Point.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Point = props => {
+  const radius = props.svgSize / 400
+
+  return (
+    <circle cx='' cy='' r={radius}
+  )
+}
+
+export default Point
--- client/neural-nets/perceptronTrainer.js
+++ client/neural-nets/perceptronTrainer.js
@@ -3,7 +3,6 @@ const _ = require('lodash')
 const Perceptron = require('./perceptron')
 
 const maxIterations = 1000000
-// const printInterval = 5000
 let a = _.random(-2, 2, true)
 let b = _.random(-50, 50)
 
@@ -21,12 +20,10 @@ function isAboveLine (x, y) {
 
 /**
  * Trains a perceptron
- * @param {*} perceptron 
+ * @param {*} perceptron  The Perceptron to train
  * @param {*} percentCompleteFn Returns the percentage complete as a parameter to this function
  */
 function train (perceptron, percentCompleteFn) {
-  /* let numCorrect = 0
-  let lastCorrect = 0 */
   for (let i = 0; i < maxIterations; i++) {
     const point = [
       _.random(-100, 100),
@@ -35,16 +32,10 @@ function train (perceptron, percentCompleteFn) {
 
     const actual = perceptron.process(point)
     const expected = isAboveLine(point[0], point[1])
-    /* if (actual === expected) numCorrect++
-    if ((i + 1) % printInterval === 0) {
-      console.log(`Correct: ${numCorrect}/${i + 1}\t\t Difference: ${numCorrect - lastCorrect}`)
-      console.log(perceptron.weightsToString())
-      lastCorrect = numCorrect
-    } */
     const difference = expected - actual
     const learningRate = 1 - (i / maxIterations)
     perceptron.adjust(point, difference, learningRate)
-    
+
     if (percentCompleteFn) {
       percentCompleteFn(i / maxIterations)
     }
@@ -54,7 +45,6 @@ function train (perceptron, percentCompleteFn) {
 function trainPerceptron (chosenA, chosenB, percentCompleteFn) {
   a = chosenA || _.random(-2, 2, true)
   b = chosenB || _.random(-50, 50)
-  // console.log(`Gradient is: ${a.toFixed(2)}x + ${b}`)
   const perceptron = new Perceptron(2)
   train(perceptron, percentCompleteFn)
   return {
