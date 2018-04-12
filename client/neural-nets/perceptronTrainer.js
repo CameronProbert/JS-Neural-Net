705fe8f94d145352a55379d49aaa8a3c0b4d84d6
Cameron Probert <c.probert@hotmail.co.nz>
2018-04-12 12:01:32 +1200

    added canvas in Display component

--- client/neural-nets/perceptronTrainer.js
+++ client/neural-nets/perceptronTrainer.js
@@ -3,9 +3,9 @@ const _ = require('lodash')
 const Perceptron = require('./perceptron')
 
 const maxIterations = 1000000
-const printInterval = 5000
-const a = _.random(-2, 2, true)
-const b = _.random(-50, 50)
+// const printInterval = 5000
+let a = _.random(-2, 2, true)
+let b = _.random(-50, 50)
 
 /**
  * Returns the y value of the function ax + b
@@ -19,9 +19,14 @@ function isAboveLine (x, y) {
   return test(x) < y ? 0 : 1
 }
 
-function train (perceptron) {
-  let numCorrect = 0
-  let lastCorrect = 0
+/**
+ * Trains a perceptron
+ * @param {*} perceptron 
+ * @param {*} percentCompleteFn Returns the percentage complete as a parameter to this function
+ */
+function train (perceptron, percentCompleteFn) {
+  /* let numCorrect = 0
+  let lastCorrect = 0 */
   for (let i = 0; i < maxIterations; i++) {
     const point = [
       _.random(-100, 100),
@@ -30,22 +35,33 @@ function train (perceptron) {
 
     const actual = perceptron.process(point)
     const expected = isAboveLine(point[0], point[1])
-    if (actual === expected) numCorrect++
+    /* if (actual === expected) numCorrect++
     if ((i + 1) % printInterval === 0) {
       console.log(`Correct: ${numCorrect}/${i + 1}\t\t Difference: ${numCorrect - lastCorrect}`)
       console.log(perceptron.weightsToString())
       lastCorrect = numCorrect
-    }
+    } */
     const difference = expected - actual
     const learningRate = 1 - (i / maxIterations)
     perceptron.adjust(point, difference, learningRate)
+    
+    if (percentCompleteFn) {
+      percentCompleteFn(i / maxIterations)
+    }
   }
 }
 
-function trainPerceptron () {
-  console.log(`Gradient is: ${a.toFixed(2)}x + ${b}`)
+function trainPerceptron (chosenA, chosenB, percentCompleteFn) {
+  a = chosenA || _.random(-2, 2, true)
+  b = chosenB || _.random(-50, 50)
+  // console.log(`Gradient is: ${a.toFixed(2)}x + ${b}`)
   const perceptron = new Perceptron(2)
-  train(perceptron)
+  train(perceptron, percentCompleteFn)
+  return {
+    perceptron,
+    a,
+    b
+  }
 }
 
 module.exports = {
