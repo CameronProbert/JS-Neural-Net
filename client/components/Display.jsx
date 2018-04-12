662c9a26ea218e3a5f02af85351aaca11e6ee533
Cameron Probert <c.probert@hotmail.co.nz>
2018-04-12 13:57:56 +1200

    added graph as svg

--- client/components/Display.jsx
+++ client/components/Display.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 const trainPerceptron = require('../neural-nets/perceptronTrainer')
 
+const svgSize = 600
+const cx = svgSize / 2
+const cy = svgSize / 2
+
+const testA = 1
+const testB = 50
+
 class Display extends React.Component {
   constructor (props) {
     super(props)
@@ -11,10 +18,14 @@ class Display extends React.Component {
     const a = perceptronData.a
     const b = perceptronData.b
 
+    // Binds
+    this.handleClick = this.handleClick.bind(this)
+
     this.state = {
-      a,
-      b,
-      perceptron
+      a: testA,
+      b: testB,
+      perceptron,
+      dataPoints: []
     }
   }
 
@@ -24,19 +35,29 @@ class Display extends React.Component {
     })
   }
 
-  handleClick () {
-
+  handleClick (e) {
+    console.log('clicked')
   }
 
   render () {
     return (
       <div className='display-wrapper'>
         <div className='display'>
-          <canvas onClick={this.handleClick} id='perceptronCanvas' width='400' height='400'></canvas>
+          <svg onClick={this.handleClick} width={svgSize} height={svgSize}>
+            <line className='axis' x1={cx} y1="0" x2={cx} y2={svgSize} />
+            <line className='axis' x1="0" y1={cy} x2={svgSize} y2={cy} />
+            <line
+              className='gradientLine'
+              x1="0" y1={cy - bound(testA, -100, testB)}
+              x2={svgSize} y2={cy - bound(testA, 100, testB)}
+            />
+          </svg>
         </div>
       </div>
     )
   }
 }
+
+const bound = (a, x, b) => Math.round((a * x + b) * (svgSize / 200))
 
 export default Display
